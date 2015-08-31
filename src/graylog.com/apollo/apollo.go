a993842a7651c351335bf51e7e4d50bfa65c0522
Lennart Koopmann <lennart@graylog.com>
2015-08-30 23:20:59 -0500

    refactored reading from REST API, read more information

--- src/graylog.com/apollo/apollo.go
+++ src/graylog.com/apollo/apollo.go
@@ -8,22 +8,31 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 	"time"
 )
 
-// TODO: multiple server instances? auto-discovery?
+// TODO: multiple server instances? auto-discovery? required with cluster stats read we already do?
+//       collect info about cpu usage, cores, io, ...? is there a lib for that? do we have it in rest?
+//       can we get cluster_id?
+//       make sure that there never is alarmcallback or output config in streams
 
 var username string
 var password string
 var url string
+var submitToken string
+
+type IncludedFile struct {
+	Name string
+	Body []byte
+}
 
 func init() {
 	flag.StringVar(&username, "user", "", "Graylog username (must have administrator permissions)")
 	flag.StringVar(&password, "password", "", "Graylog password")
 	flag.StringVar(&url, "url", "", "URL of graylog-server REST URL. (Example: http://graylog.example.org:12900)")
+	flag.StringVar(&submitToken, "submit", "", "Provie an Apollo token to submit directly. (optional)")
 }
 
 func main() {
@@ -39,19 +48,32 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 	log.Println("Starting up.")
 
-	// Get all metrics and write to file.
-	metrics := readMetrics()
-	log.Printf("Successfully read %vkb of metrics from Graylog.", len(metrics)/1024)
-	err := ioutil.WriteFile("metrics.json", metrics, 0644)
-	check(err)
-
 	// Zip up the files.
-	zipIt(metrics)
-	log.Println("Wrote bundle .zip file.")
+	var files = []IncludedFile{
+		{"metrics.json", readResourceJson("system/metrics")},
+		{"system.json", readResourceJson("system")},
+		{"system_jvm.json", readResourceJson("system/jvm")},
+		{"system_stats.json", readResourceJson("system/stats")},
+		{"cluster_stats.json", readResourceJson("system/cluster/stats")},
+		{"cluster_nodes.json", readResourceJson("system/cluster/nodes")},
+		{"services.json", readResourceJson("system/serviceManager")},
+		{"journal.json", readResourceJson("system/journal")},
+		{"buffers.json", readResourceJson("system/buffers")},
+		{"notifications.json", readResourceJson("system/notifications")},
+		{"throughput.json", readResourceJson("system/throughput")},
+		{"streams.json", readResourceJson("streams")},
+		{"streams_throughput.json", readResourceJson("streams/throughput")},
+	}
 
-	// Clean up tmp files.
-	log.Println("Cleaning up temporary files.")
-	os.Remove("metrics.json")
+	filename := zipIt(files)
+	log.Printf("Wrote bundle to file: %v\n", filename)
+
+	// Submit to Apollo directly?
+	if len(submitToken) > 0 {
+		log.Printf("Submission to Apollo service was requested. Submitting with token [%v].\n", submitToken)
+
+		// TODO: actually build this.
+	}
 
 	log.Println("Finished.")
 }
@@ -77,8 +99,8 @@ func getHTTPRequest(path string) (*http.Client, *http.Request) {
 	return client, req
 }
 
-func readMetrics() []byte {
-	client, req := getHTTPRequest("system/metrics")
+func readResourceJson(path string) []byte {
+	client, req := getHTTPRequest(path)
 
 	resp, err := client.Do(req)
 
@@ -98,20 +120,15 @@ func readMetrics() []byte {
 
 	resp.Body.Close()
 
+	log.Printf("Successfully read %v bytes [%v] from Graylog.", len(result), path)
+
 	return result
 }
 
-func zipIt(metrics []byte) {
+func zipIt(files []IncludedFile) string {
 	buf := new(bytes.Buffer)
 	zipWriter := zip.NewWriter(buf)
 
-	// Add files to the archive.
-	var files = []struct {
-		Name string
-		Body []byte
-	}{
-		{"metrics.json", metrics},
-	}
 	for _, file := range files {
 		zipFile, err := zipWriter.Create(file.Name)
 		if err != nil {
@@ -130,7 +147,9 @@ func zipIt(metrics []byte) {
 	t := time.Now()
 	finalName := fmt.Sprintf("graylog_apollo_bundle-%d-%02d-%02dT%02d-%02d-%02d.zip", t.Year(), t.Month(), t.Day(),
 		t.Hour(), t.Minute(), t.Second())
-	ioutil.WriteFile(finalName, buf.Bytes(), 0644) // TODO: include timestamp
+	ioutil.WriteFile(finalName, buf.Bytes(), 0644)
+
+	return finalName
 }
 
 func check(e error) {
