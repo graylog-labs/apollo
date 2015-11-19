28a5dbf28bf9b22b829dbdb614a10d1d1a9ddfaa
Thomas Desrosiers <thomasdesr@gmail.com>
2015-11-19 16:39:59 -0500

    Fix the build script to be more portable

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-GOPATH=/Users/lennart/workspace/graylog2/apollo-collector GOOS=darwin GOARCH=amd64 /usr/local/go/bin/go install graylog.com/apollo
-GOPATH=/Users/lennart/workspace/graylog2/apollo-collector GOOS=linux GOARCH=amd64 /usr/local/go/bin/go install graylog.com/apollo
-GOPATH=/Users/lennart/workspace/graylog2/apollo-collector GOOS=windows GOARCH=amd64 /usr/local/go/bin/go install graylog.com/apollo
+OSs="darwin linux windows"
+ARCHs="386 amd64"
 
-GOPATH=/Users/lennart/workspace/graylog2/apollo-collector GOOS=darwin GOARCH=386 /usr/local/go/bin/go install graylog.com/apollo
-GOPATH=/Users/lennart/workspace/graylog2/apollo-collector GOOS=linux GOARCH=386 /usr/local/go/bin/go install graylog.com/apollo
-GOPATH=/Users/lennart/workspace/graylog2/apollo-collector GOOS=windows GOARCH=386 /usr/local/go/bin/go install graylog.com/apollo
+for os in $OSs; do
+	for arch in $ARCHs; do
+		go build -o apollo-collector-$os-$arch .
+	done
+done
